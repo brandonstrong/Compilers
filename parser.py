dc97f3c927a2f75d74f051d6935443f8d3335838
hianxi3ty <hianxi3ty@unseen.is>
2016-02-19 16:04:24 -0700

    Built expression definitions

--- parser.py
+++ parser.py
@@ -21,6 +21,50 @@ def p_program_decl(p):
     | var_decl
     | empty'''
 
+def p_expressions_expr(p):
+    'expr : expr_prefix factor'
+
+def p_expressions_expr_prefix(p):
+    '''expr_prefix : expr_prefix factor addop
+    | empty'''
+
+def p_expressions_factor(p):
+    'factor : factor_prefix postfix_expr'
+
+def p_expressions_factor_prefix(p):
+    '''factor_prefix : factor_prefix postfix_expr mulop
+    | empty'''
+
+def p_expressions_postfix_expr(p):
+    '''postfix_expr : primary
+    | call_expr'''
+
+def p_expressions_call_expr(p):
+    'call_expr : id ( expr_list )'
+
+def p_expressions_expr_list(p):
+    '''expr_list : expr expr_list_tail
+    | empty'''
+
+def p_expressions_expr_list_tail(p):
+    '''expr_list_tail : , expr expr_list_tail
+    | empty'''
+
+def p_expressions_primary(p):
+    '''primary : ( expr )
+    | id
+    | INTLITERAL
+    | FLOATLITERAL'''
+
+def p_expressions_addop(p):
+    '''addop : +
+    | -'''
+
+def p_expressions_mulop(p):
+    '''mulop : *
+    | /'''
+
+
 def p_error(p):
     print('Syntax error in input!')
 
