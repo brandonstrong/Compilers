ab638f1222c6827768f450a18378e9571fa2c029
gr33nh00d <sawyerpayne1@gmail.com>
2016-04-04 16:08:41 -0600

    adding in stuff from lab4

---
+++ lab4/lab4.py
@@ -0,0 +1,31 @@
+
+r = open('input.txt','r')
+print(r.readline())
+print(r.readline())
+
+k = r.readline()
+while k != '':
+    print(k.split(" "))
+    if len(k)==1:
+
+    if len(k)==2:
+
+    if len(k)==3:
+
+    k=r.readline()
+
+if node.opcode=="LABEL"
+
+if node.opcode=="LINK"
+
+if node.opcode=="STOREI"
+
+if node.opcode=="MULTI"
+
+if node.opcode=="ADDI"
+
+if node.opcode=="DIVI"
+
+if node.opcode=="WRITEI"
+
+if node.opcode=="RET"
